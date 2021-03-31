b6c04e98c7cc84fd0606bd244928f93c7c214817
LIU JIAN <onanying@126.com>
2021-03-31 10:34:36 +0800

    feat: up

--- controllers/user.go
+++ controllers/user.go
@@ -1,34 +1,25 @@
 package controllers
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/mix-go/web-skeleton/globals"
-    "github.com/mix-go/web-skeleton/models"
-    "net/http"
-    "time"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type UserController struct {
 }
 
 func (t *UserController) Add(c *gin.Context) {
-    // 网页
-    if c.Request.Method == http.MethodGet {
-        c.HTML(http.StatusOK, "user_add.tmpl", gin.H{
-            "title": "User add",
-        })
-        c.Abort()
-        return
-    }
+	// 网页
+	if c.Request.Method == http.MethodGet {
+		c.HTML(http.StatusOK, "user_add.tmpl", gin.H{
+			"title": "User add",
+		})
+		c.Abort()
+		return
+	}
 
-    db := globals.Gorm()
-    if err := db.Create(&models.User{
-        Name:     c.Request.PostFormValue("name"),
-        CreateAt: time.Now(),
-    }).Error; err != nil {
-        c.String(http.StatusInternalServerError, "<html><h1>%s</h1></html>", "Add error!")
-        c.Abort()
-        return
-    }
-    c.String(http.StatusInternalServerError, "<html><h1>%s</h1></html>", "Add ok!")
+	// 执行数据库操作
+	// ...
+
+	c.String(http.StatusInternalServerError, "<html><h1>%s</h1></html>", "Add ok!")
 }
